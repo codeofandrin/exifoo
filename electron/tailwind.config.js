ee03eb111bdd21e6cb72149ab77e344f2fde65e0
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-11-30 14:40:11 +0100

    Add colors and fonts

--- electron/tailwind.config.js
+++ electron/tailwind.config.js
@@ -1,15 +1,18 @@
 import flowbite from "flowbite-react/tailwind"
 import defaultTheme from "tailwindcss/defaultTheme"
+import exifooTheme from "./theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/renderer/index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
     theme: {
-        extend: {},
+        extend: {
+            colors: exifooTheme.colors
+        },
         screens: {
             xs: "360px",
             ...defaultTheme.screens
         }
     },
-    plugins: [flowbite.plugin(), require("daisyui")]
+    plugins: [flowbite.plugin()]
 }
