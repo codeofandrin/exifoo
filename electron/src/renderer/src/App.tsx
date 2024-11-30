c009c7f1889c8545e8653ef301e61112ac6b66df
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-11-30 15:29:19 +0100

    Remove unused components, contexts and providers

--- electron/src/renderer/src/App.tsx
+++ electron/src/renderer/src/App.tsx
@@ -1,12 +1,9 @@
 import ProviderComposer from "./providers/ProviderComposer"
-import YearOptionProvider from "./providers/YearOptionProvider"
-import TimeOptionProvider from "./providers/TimeOptionProvider"
-import CustomTextProvider from "./providers/CustomTextProvider"
 import Header from "./components/Header"
 
 export default function App() {
   return (
-    <ProviderComposer components={[YearOptionProvider, TimeOptionProvider, CustomTextProvider]}>
+    <ProviderComposer components={[]}>
       <>
         <Header />
       </>
