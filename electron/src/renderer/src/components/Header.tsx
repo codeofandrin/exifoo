ea4e2ff681432b59c1291915e4ef911e18eff3a8
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-11-30 15:21:58 +0100

    Add header section

---
+++ electron/src/renderer/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import useAboutModal from "../contexts/AboutModalContext"
+import ImgAppLogoLarge from "../assets/images/exifoo_logo_large.png"
+import SVGInfo from "../assets/icons/Info.svg?react"
+
+export default function Header() {
+  const { openModal } = useAboutModal()
+
+  function handleAboutClick() {
+    openModal()
+  }
+
+  return (
+    <div className="flex h-20 items-center justify-between">
+      {/* App Logo */}
+      <div>
+        <a>
+          <img src={ImgAppLogoLarge} alt="exifoo" className="w-32" />
+        </a>
+      </div>
+      {/* Icon Section */}
+      <div>
+        {/* About */}
+        <button onClick={handleAboutClick}>
+          <SVGInfo className="h-6 w-6 text-neutral-800" />
+        </button>
+      </div>
+    </div>
+  )
+}
