115a1eeec444a9aa54e506a0ba3fd12f497e34b4
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-12-22 00:35:21 +0100

    Add encryption for safe storage

--- backend/exifoo/license.py
+++ backend/exifoo/license.py
@@ -1,14 +1,17 @@
 import json
+import os
 from typing_extensions import NamedTuple
 
 import keyring
+from cryptography.fernet import Fernet
 
 from .errors import APIException, APIErrorType, APIExceptionDetail
 from .enums import AppAccessType
 
 
 KEYRING_SERVICE_NAME = "exifoo Safe Storage"
-KEYRING_USERNAME = "exifoo License"
+KEYRING_LICENSE_USERNAME = "exifoo License"
+STORAGE_KEY = os.getenv("STORAGE_KEY")
 
 
 class _AppAccess:
@@ -32,6 +35,14 @@ class _AppAccess:
 app_access = _AppAccess()
 
 
+def _encrypt(data: str) -> str:
+    return Fernet(STORAGE_KEY).encrypt(data.encode()).decode()
+
+
+def _decrypt(token: str) -> str:
+    return Fernet(STORAGE_KEY).decrypt(token.encode()).decode()
+
+
 class License(NamedTuple):
     key: str
     instance_id: str
@@ -39,24 +50,24 @@ class License(NamedTuple):
 
 def store_license(*, key: str, instance_id: str) -> None:
     license_info = {"key": key, "instance_id": instance_id}
-    keyring.set_password(KEYRING_SERVICE_NAME, KEYRING_USERNAME, json.dumps(license_info))
+    encrypted = _encrypt(json.dumps(license_info))
+    keyring.set_password(KEYRING_SERVICE_NAME, KEYRING_LICENSE_USERNAME, encrypted)
 
 
 def get_license() -> License:
-    raw_license_info = keyring.get_password(KEYRING_SERVICE_NAME, KEYRING_USERNAME)
+    raw_license_info = keyring.get_password(KEYRING_SERVICE_NAME, KEYRING_LICENSE_USERNAME)
     if raw_license_info is None:
         raise APIException(
             status_code=400,
             error_code=APIErrorType.license_not_found,
             msg="License key not found",
             detail=APIExceptionDetail(
-                msg="License key could not be found in safe storage", item=KEYRING_USERNAME
+                msg="License key could not be found in safe storage", item=KEYRING_LICENSE_USERNAME
             ),
         )
-
-    license_info = json.loads(raw_license_info)
+    license_info = json.loads(_decrypt(raw_license_info))
     return License(key=license_info["key"], instance_id=license_info["instance_id"])
 
 
 def delete_license() -> None:
-    keyring.delete_password(KEYRING_SERVICE_NAME, KEYRING_USERNAME)
+    keyring.delete_password(KEYRING_SERVICE_NAME, KEYRING_LICENSE_USERNAME)
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+import env
 from exifoo.api import app
 
 
