b2331c490eec27aa97ab17b70d832132d533fe71
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-12-23 16:12:39 +0100

    Add free trial banner on bottom right

---
+++ electron/src/renderer/src/components/main/FreeTrialBanner.tsx
@@ -0,0 +1,32 @@
+import ExternalLink from "../common/ExternalLink"
+import { WebsiteLinks } from "../../utils/constants"
+import { useActivateLicenseStore } from "../../store/main/useActivateLicenseStore"
+import SVGAlertTriangle from "../../assets/icons/AlertTriangle.svg?react"
+
+export function FreeTrialBanner() {
+  const bulletPoint = <span className="mx-1">•</span>
+  const { setIsOpen: setIsActivateLicenseOpen } = useActivateLicenseStore()
+
+  function handleActivate() {
+    setIsActivateLicenseOpen(true)
+  }
+
+  return (
+    <div className="absolute bottom-0 right-0 mb-5 mr-5 flex items-center rounded-lg bg-neutral-700 px-3 py-2 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.5)_0px_3px_7px_-3px]">
+      <SVGAlertTriangle className="w-5 text-yellow-400" />
+      <p className="ml-2 text-xs text-neutral-300">
+        <span className="font-semibold">No license</span>
+        {bulletPoint}
+        <button
+          className="underline decoration-transparent transition-colors duration-200 hover:decoration-inherit"
+          onClick={handleActivate}>
+          Activate
+        </button>
+        {bulletPoint}
+        <ExternalLink href={WebsiteLinks.license} color="silent">
+          Buy
+        </ExternalLink>
+      </p>
+    </div>
+  )
+}
--- electron/src/renderer/src/pages/Main.tsx
+++ electron/src/renderer/src/pages/Main.tsx
@@ -2,6 +2,7 @@ import Header from "../components/main/Header"
 import RenameForm from "../components/main/rename-photos/RenameForm"
 import Options from "../components/main/rename-options/RenameOptions"
 import Footer from "../components/main/Footer"
+import { FreeTrialBanner } from "../components/main/FreeTrialBanner"
 import ActivateLicense from "../components/main/ActivateLicense"
 import ProviderComposer from "../providers/main/ProviderComposer"
 import DateOptionsProvider from "../providers/main/DateOptionsProvider"
@@ -9,11 +10,16 @@ import TimeOptionsProvider from "../providers/main/TimeOptionsProvider"
 import CustomTextProvider from "../providers/main/CustomTextProvider"
 import { registerUpdaterEvents } from "../lib/update-events"
 import { useActivateLicenseStore } from "../store/main/useActivateLicenseStore"
+import { useAppStore } from "../store/useAppStore"
+import { LicenseType } from "../utils/enums"
 
 registerUpdaterEvents()
 
 export default function Main() {
   const { isOpen: isActivateLicenseOpen, setIsOpen: setIsActivateLicenseOpen } = useActivateLicenseStore()
+  const { license_type } = useAppStore()
+
+  const isDemo = license_type === LicenseType.demo
 
   return (
     <>
@@ -28,6 +34,7 @@ export default function Main() {
               <Options />
             </div>
             <Footer />
+            {isDemo && <FreeTrialBanner />}
           </div>
         </ProviderComposer>
       )}
